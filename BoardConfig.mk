ab0d4db91dae78be284be9b2556433d1276ebac6
xplodwild <xplod@ouverta.fr>
2013-09-15 21:38:28 +0200

    BoardConfig: Add touchboost proper path
    Change-Id: I020a7a131c7e3c61687665a2a0c0d738da5aa751

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,7 @@ TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
+TARGET_MPDECISION_BOOST_SOCKET := /dev/socket/mpdecision/touchboost
 
 TARGET_NO_BOOTLOADER := true
 
