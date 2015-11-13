c03f913df0b414cf3b7f0e3e6d9534f681b73e7c
kldoc <fred.berbigier@gmail.com>
2015-11-13 19:21:56 +0800

    force dex2oat not to use swap file
    force dex2oat not to use swap file
    Based on https://android.googlesource.com/device/asus/flo/+/9e9ab1ce70d87d684e848dc41fb6f81ba3f591b8
    
    Change-Id: I750cea2aea8e975413aed175105bb7715aed5c3f

--- device.mk
+++ device.mk
@@ -153,6 +153,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #Upto 3 layers can go through overlays
 PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false
+
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_TAGS += dalvik.gc.type-precise
