9aac80acd786e12363fc8548f231065e22dd99ac
kldoc <fred.berbigier@gmail.com>
2016-03-12 09:01:09 +0800

    add LMY48M fingerprint and description
    Change-Id: I563aa0af71fc62d91b1f5fd3a9a63fe7bba39af3

--- omni_mako.mk
+++ omni_mako.mk
@@ -36,6 +36,10 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2167285 release-keys"
+
 # Kernel inline build
 TARGET_KERNEL_CONFIG := mako_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
