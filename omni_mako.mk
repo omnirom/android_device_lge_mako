e13155d1deeb16f4e529d5fced7a24e7eb29158b
kldoc <fred.berbigier@gmail.com>
2016-08-26 06:46:51 +0800

    mako: switch to default toolchain
    Change-Id: I6f855697ee13eeb6bf9e45153f1f1848eb66de96

--- omni_mako.mk
+++ omni_mako.mk
@@ -42,7 +42,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Kernel inline build
 TARGET_KERNEL_CONFIG := mako_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 TARGET_VARIANT_CONFIG := mako_defconfig
 TARGET_SELINUX_CONFIG := mako_defconfig
 
